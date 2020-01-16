9462d02e7fe18aac8abd8ba8fb5bc1c07bc8d732
Eugene Kaygorodov <gekak94@gmail.com>
2020-01-16 21:04:45 +0500

    eslint, codeclimate, travis settings

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 
+import test from '..';
+
 const program = require('commander');
 
 program
   .version('0.1.0')
   .description('Compares two configuration files and shows a difference')
-  .option('-f, --format <type>', 'output format')
-  .action(() => console.log('it has run'))
+  .option('-f, --format <type>', 'Output format')
+  .action(test)
   .parse(process.argv);
