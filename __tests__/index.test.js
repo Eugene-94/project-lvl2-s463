2e44ef0587b402fe2fdcbd3882a4d40d72e41d01
Eugene Kaygorodov <gekak94@gmail.com>
2020-02-03 21:19:58 +0500

    step 7

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,10 +5,13 @@ import gendiff from '../src';
 test.each([
   ['.json', 'tree', 'result-tree.txt'],
   ['.json', 'plain', 'result-plain.txt'],
+  ['.json', 'json', 'result-json.txt'],
   ['.yml', 'tree', 'result-tree.txt'],
   ['.yml', 'plain', 'result-plain.txt'],
+  ['.yml', 'json', 'result-json.txt'],
   ['.ini', 'tree', 'result-tree.txt'],
   ['.ini', 'plain', 'result-plain.txt'],
+  ['.ini', 'json', 'result-json.txt'],
 ])('.gendiff(%s, %s, %s)', (extension, format, result) => {
   const pathBefore = path.resolve(__dirname, `__fixtures__/before${extension}`);
   const pathAfter = path.resolve(__dirname, `__fixtures__/after${extension}`);
--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,10 +1,12 @@
 import treeRender from './tree';
 import plainRender from './plain';
+import jsonRender from './json';
 
 export default (ast, formatter) => {
   const formatsType = {
     tree: treeRender,
     plain: plainRender,
+    json: jsonRender,
   };
 
   const render = formatsType[formatter];
